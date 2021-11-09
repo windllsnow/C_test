bb547c2bde9cc282caaa7111d9a2efc5c6372c10
windllsnow <87889501+windllsnow@users.noreply.github.com>
2021-11-10 01:59:46 +0800

    Add files via upload

---
+++ Ccc/ct0004.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    char ch= 'A';
+    char ch1=70;
+    char ch2='2';
+    char ch3=1;//白色笑臉
+    printf("ch= %c \n",ch); //印 字元變數
+    printf("ch1= %c\n",ch1);
+    printf("ASCII of ch=%d \n",ch);
+    printf("ASCII of ch1=%d \n",ch1);
+    printf("ch2= %c\n",ch2);
+    printf("ASCII of ch2=%d \n",ch2);
+    printf("ch3= %c\n",ch3);
+    printf("ASCII of ch3=%d \n",ch3);
+
+    system ("pause");
+    return 0;
+}
